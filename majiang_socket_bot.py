89afed2977156698c352cc00b6583b89053f1b6b
hidacow <68262945+hidacow@users.noreply.github.com>
2024-09-01 15:13:04 +0800

    minor update

--- majiang_socket_bot.py
+++ majiang_socket_bot.py
@@ -27,6 +27,7 @@ class MajiangBot:
     server: str = ""
     authpath: str = ""
     socketpath: str = ""
+    modelpath: str = ""
     bot: Bot = None
     game: GameState = None
     myuid = ""
@@ -36,20 +37,21 @@ class MajiangBot:
     room = ""
     session: requests.Session = None
 
-    def __init__(self, setting: MajiangBotSetting, room=""):
+    def __init__(self, setting: MajiangBotSetting, room="", botname=""):
         self.server = setting.server
         self.authpath = setting.apppath + "server/auth/"
         self.socketpath = setting.apppath + "server/socket.io/"
+        self.modelpath = setting.modelpath
         self.bot = get_bot(setting.modelpath)
         self.game = GameState(self.bot)
         self.room = room
+        self.myname = botname if botname else generate_random_name()
 
     def loop(self):
         self.sio.wait()
 
     def start(self):
         # self.session.verify = False
-        self.myname = generate_random_name(4)
         print("Starting bot:", self.myname)
         self.session = requests.Session()
         r = self.session.post(
@@ -61,7 +63,7 @@ class MajiangBot:
                 "Failed to auth on the server: " + self.server + self.authpath
             )
         time.sleep(1)
-        self.sio = socketio.Client(http_session=self.session)
+        self.sio = socketio.Client(http_session=self.session, reconnection_attempts=3)
         self.callbacks()
         self.sio.connect(self.server, socketio_path=self.socketpath)
         self.loop()
@@ -71,10 +73,6 @@ class MajiangBot:
         @self.sio.event
         def connect():
             print(self.myname, "Connected to server:", self.server)
-            if self.room:
-                self.sio.emit("ROOM", self.room)
-            else:
-                self.sio.emit("ROOM")
 
         @self.sio.event
         def connect_error(data):
@@ -94,15 +92,39 @@ class MajiangBot:
             else:
                 if self.myuid and "offline" in data.keys():
                     print(self.myname, "get kicked out")
-                    self.sio.disconnect()
+                    self.is_in_room = False
+                    # self.sio.start_background_task(find_room)
+                    # self.sio.disconnect()
                 else:
                     self.myuid = data["uid"]
-                # print(self.myname,"User ID:", self.myuid)
+                    if self.room:
+                        print(self.myname, "Joining room:", self.room)
+                        self.sio.emit("ROOM", self.room)
+                    else:
+                        # self.sio.start_background_task(find_room)
+                        pass
+
 
         @self.sio.on("ROOM")
         def on_room(data):
             print(self.myname, "ROOM received:", data)
             self.is_in_room = True
+
+        # Modded Server: <ROOMS> command
+        # You can mod the original server to add this command
+        # The server should return a list of rooms that is available to join
+
+        # @self.sio.on("ROOMS")
+        # def on_rooms(data):
+        #     print("ROOMS received:", data)
+        #     if not data:
+        #         print("No room can join.")
+        #         return
+        #     if not self.is_in_room:
+        #         print("Joining room:", data[0])
+        #         self.sio.emit("ROOM", data[0])
+        #     else:
+        #         print("Already in a room.")
 
         @self.sio.on("START")
         def on_start():
@@ -111,9 +133,15 @@ class MajiangBot:
 
         @self.sio.on("END")
         def on_end(data):
-            print(self.myname, "END received:", data)
+            print(self.myname, "END received")
             self.is_in_game = False
             self.is_in_room = False
+            # save logs
+            # fn = f"logs/{self.myname}_{int(time.time())}_log.json"
+            # import json
+
+            # with open(fn, "w", encoding="utf-8") as json_file:
+            #     json.dump(data, json_file, ensure_ascii=False)
             self.sio.disconnect()
 
         @self.sio.on("ERROR")
@@ -130,8 +158,19 @@ class MajiangBot:
             # print(game.input(msg))
             mjai_react = self.game.input(data)
             reaction = self.game.trans_mjai_react(mjai_react)
-            print(mjai_react, reaction)
+            print(f"[{self.myname}]", mjai_react, reaction)
+            # print(f"[{self.myname}](tehai)", self.game.kyoku_state.my_tehai)
+            if self.game.last_reaction_time:
+                print("thought", f"{self.game.last_reaction_time}", "s")
+                # time.sleep(self.game.last_reaction_time)
             self.sio.emit("GAME", reaction)
+
+        # def find_room():
+        #     while not self.is_in_room:
+        #         print(self.myname, "Requesting rooms")
+        #         self.sio.emit("ROOMS")
+        #         time.sleep(5)
+        #     return
 
 
 if __name__ == "__main__":
@@ -162,9 +201,16 @@ if __name__ == "__main__":
     setting = MajiangBotSetting(
         server=args.server, apppath=args.apppath, modelpath=args.modelpath
     )
-    bots = [MajiangBot(setting, args.room) for _ in range(args.number)]
-    threads = [threading.Thread(target=b.start) for b in bots]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+    try:
+        while True:
+            bots = [
+                MajiangBot(setting, args.room, f"Mortal_{chr(ord('A')+_)}")
+                for _ in range(args.number)
+            ]
+            threads = [threading.Thread(target=b.start) for b in bots]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+    except KeyboardInterrupt:
+        exit()
